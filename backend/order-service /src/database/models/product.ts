75cf5af2d50ab77e3d2680cd6467ad18f8732406
65070188Mananchaya <97225144+DraftManan@users.noreply.github.com>
2025-03-24 00:15:52 +0700

    update service

--- backend/order-service /src/database/models/product.ts
+++
@@ -1,9 +0,0 @@
-import { Decimal } from "@prisma/client/runtime/library";
-
-export interface products {
-  id: string;
-  name: string;
-  description: string | null;
-  price: string; 
-  createdAt: Date;
-}
